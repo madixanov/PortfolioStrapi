8cb99abc31cdafcd8c8d87425e230ea7addc2bbd
ykDev <yoqub.madixanov@gmail.com>
2026-04-20 17:01:31 +0500

    Fixing CORS

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,18 @@
 import type { Core } from '@strapi/strapi';
 
 const config: Core.Config.Middlewares = [
+  'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'https://ykdev.netlify.app',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: '*',
+    },
+  },
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
